e3ef1b3d253b40b4153f5b2a1a211d10f9098ae1
NanYe <ynyz2003@gmail.com>
2020-07-08 22:23:30 -0600

    update

--- src/application/DateCounter.java
+++ src/application/DateCounter.java
@@ -12,6 +12,11 @@ public class DateCounter {
 	
 //	count how many days since userinputdate until today (include today).
 	public int countDays() {
+//		The Epoch Day count is a simple incrementing count of days where day 0 is 1970-01-01 (ISO). This definition is the same for all chronologies, enabling conversion.
+		long todayEpochDay = today.toEpochDay();
+		long userInputDateEpochDay = userInputDate.toEpochDay();
+		
+		long countDays = todayEpochDay-userInputDateEpochDay;
 		
 		return 0;
 	}
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.time.LocalDate;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		DateCounter dateCounter = new DateCounter(LocalDate.of(2020, 7, 1));
+		
+		dateCounter.countDays();
+		
 	}
 
 }
